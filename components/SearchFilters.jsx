2ce2fa096b06f5f81adf90a4f79de0c7b029616b
Edward Ho <pythoned77@gmail.com>
2022-09-01 21:55:27 +0800

    added search filter buttons with data

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -12,9 +12,31 @@ import {
 import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
-  return <div>searchFilters</div>;
+  const [filters, setFilters] = useState(filterData);
+
+  return (
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
 };
 
 export default SearchFilters;
