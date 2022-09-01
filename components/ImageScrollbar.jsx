102b9de00a8f787c79b1ed9fe94f3da9783df27c
Edward Ho <pythoned77@gmail.com>
2022-09-01 23:58:57 +0800

    added image scroll bar

---
+++ components/ImageScrollbar.jsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import Image from "next/image";
+import { Box, Flex, Icon } from "@chakra-ui/react";
+import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Flex justifyContent="center" alignItems="center" marginRight="1">
+      <Icon
+        as={FaArrowCircleLeft}
+        onClick={scrollPrev}
+        fontSize="2x1"
+        cursor="pointer"
+      />
+    </Flex>
+  );
+};
+const ImageScrollbar = ({ data }) => {
+  return;
+  <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    {data.map((item) => (
+      <Box key={item.id} width="910px" itemId={item.id} overflow="hidden" p="1">
+        <Image
+          alt="property"
+          placeholder="blue"
+          blurDataURL={item.url}
+          src={item.url}
+          width={1000}
+          height={500}
+          sizes="(max-width:500px) 100px, (max-width): 1023px 400px, 1000px"
+        />
+      </Box>
+    ))}
+  </ScrollMenu>;
+};
+
+export default ImageScrollbar;
