9a7e16c925ec440afa682e7555bbbc6f7903be55
Edward Ho <pythoned77@gmail.com>
2022-09-01 17:26:39 +0800

    added pricing for the properties under property file

--- components/Property.jsx
+++ components/Property.jsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 import { Box, Flex, Text, Avatar } from "@chakra-ui/react";
 import { FaBed, FaBath } from "react-icons/fa";
 import { BsGridFill } from "react-icons/bs";
-import { Goverified } from "react-icons/go";
+import { GoVerified } from "react-icons/go";
 import millify from "millify";
+
+import DefaultImage from "../assets/images/defaulthouse.jpg";
 
 const Property = ({
   property: {
@@ -21,7 +23,36 @@ const Property = ({
   },
 }) => (
   <Link href={`/property/${externalId}`} passHref>
-    {title}
+    <Flex
+      flexWrap="wrap"
+      w="420px"
+      p="5"
+      paddingTop="0"
+      justifyContent="flex-start"
+      cursor="pointer"
+    >
+      <Box>
+        <Image
+          src={coverPhoto ? coverPhoto.url : DefaultImage}
+          width={400}
+          height={260}
+          alt="house"
+        />
+      </Box>
+      <Box w="full">
+        <Flex paddingTop="2" alignItems="center" justifyContent="space-between">
+          <Flex alignItems="center">
+            <Box paddingRight="3" color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight="bold" fontSize="lg">
+              AED {price}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    </Flex>
   </Link>
 );
 
--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["cdn-cms.pgimgs.com"],
+    domains: ["bayut-production.s3.eu-central-1.amazonaws.com"],
   },
 };
